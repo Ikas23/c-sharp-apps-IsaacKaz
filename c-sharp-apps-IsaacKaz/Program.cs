c40a5bba56bff6c3f77467104c59153d7d155540
Isaac Kazoula <isaac.kazoula@icloud.com>
2024-05-27 21:26:40 +0300

    homework

--- c-sharp-apps-IsaacKaz/Program.cs
+++ c-sharp-apps-IsaacKaz/Program.cs
@@ -1,3 +1,15 @@
 ﻿using c_sharp_apps_IsaacKaz.Common_system_shared;
+namespace c_sharp_apps_IsaacKaz.object_basic;
 
-ProcessManager.MethodeCall();
+//ProcessManager.MethodeCall();
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        RunApp.Demo();
+    }
+}
+
+
+
---
+++ c-sharp-apps-IsaacKaz/object-basic/Cola.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_IsaacKaz.object_basic
+{
+    public class Cola
+    {
+        public string ColaName { get; set; }
+        public string DateOfExpiration { get; set; }
+        public int Price { get; set; }
+
+        public Cola(int price, string colaName, string dateOfExpiration)
+        {
+            this.ColaName = colaName;
+            this.DateOfExpiration = dateOfExpiration;
+            this.Price = price;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Price = {Price}, Name = {ColaName}, DateOfExpiration = {DateOfExpiration}");
+        }
+    }
+
+    public class Box
+    {
+        Cola[] C = new Cola[6];
+
+        public int BottlePerBox { get; set; }
+        public int BoxPrice { get; set; }
+        public int NumOfBox { get; set; }
+
+        public Box(int bottlePerBox, int boxPrice)
+        {
+            this.BottlePerBox = bottlePerBox;
+            this.BoxPrice = boxPrice;
+
+            
+            for (int i = 0; i < C.Length; i++)
+            {
+                C[i] = new Cola(10, "coca cola", "23.4.25");
+            }
+        }
+
+        public void BoxesPrice(int numOfBox)
+        {
+            Console.WriteLine("The price is: " + numOfBox * BoxPrice);
+        }
+    }
+
+    public class RunApp
+    {
+        public static void Demo()
+        {
+            Cola cola = new Cola(10, "coca cola", "23.4.25");
+            cola.Display();
+
+            Box box = new Box(6, 60);
+            box.BoxesPrice(5);
+        }
+    }
+
+
+}
+
